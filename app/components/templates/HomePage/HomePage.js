04a6a73d4b31a60d79d65d2703d51a24452d1275
sudhir <sudhir.madaan@gmail.com>
2018-10-22 08:51:38 +0530

    eslintrc added

--- app/components/templates/HomePage/HomePage.js
+++ app/components/templates/HomePage/HomePage.js
@@ -1,16 +1,16 @@
 // @flow
-import { Component } from "react";
-import get from "lodash/get";
-import Layout from "../Layout";
-import enhance from "../../../lib/dynamicStore";
-import initialActions from "./HomePage.actions";
-import HeadTag from "../../atoms/HeadTag";
-import Image from "../../atoms/Image";
-import saga from "./HomePage.saga";
-import reducer from "./HomePage.reducer";
-import TripDetailsCard from "../../molecules/TripDetailsCard";
-import Modal from "../../molecules/Modal";
-import Button from "../../atoms/Button";
+import { Component } from 'react';
+import get from 'lodash/get';
+import Layout from '../Layout';
+import enhance from '../../../lib/dynamicStore';
+import initialActions from './HomePage.actions';
+import HeadTag from '../../atoms/HeadTag';
+import Image from '../../atoms/Image';
+import saga from './HomePage.saga';
+import reducer from './HomePage.reducer';
+import TripDetailsCard from '../../molecules/TripDetailsCard';
+import Modal from '../../molecules/Modal';
+import Button from '../../atoms/Button';
 
 type Props = {};
 
@@ -18,8 +18,8 @@ class HomePage extends Component<Props> {
   static async getInitialProps(props) {}
   static defaultProps = {
     seoData: {
-      homePageMetaDesc: "",
-      homePagePageTitle: "",
+      homePageMetaDesc: '',
+      homePagePageTitle: '',
       seoMetaDesc: undefined
     },
     editorialData: {},
@@ -37,17 +37,17 @@ class HomePage extends Component<Props> {
   };
 
   addWidgets = () => {
-    alert("ran after popup is opened");
-    if (document.getElementById("widget")) return;
-    const script = document.createElement("script");
-    const scriptContainer = document.createElement("div");
-    scriptContainer.id = "widget";
-    script.type = "text/javascript";
+    alert('ran after popup is opened');
+    if (document.getElementById('widget')) return;
+    const script = document.createElement('script');
+    const scriptContainer = document.createElement('div');
+    scriptContainer.id = 'widget';
+    script.type = 'text/javascript';
     script.async = true;
-    script.src = "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css";
-    document.getElementsByTagName("script")[0].parentNode.appendChild(scriptContainer);
+    script.src = 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css';
+    document.getElementsByTagName('script')[0].parentNode.appendChild(scriptContainer);
     scriptContainer.appendChild(script);
-    alert("Widget Script has been downloaded..see network tab in Dev Tools");
+    alert('Widget Script has been downloaded..see network tab in Dev Tools');
   };
 
   render() {
@@ -60,7 +60,7 @@ class HomePage extends Component<Props> {
     } else {
       return (
         <Layout title="home" className="row" id="content-wrapper">
-          <HeadTag description="Home Page Description" title={story ? story.title : "Home Page"} />
+          <HeadTag description="Home Page Description" title={story ? story.title : 'Home Page'} />
           <div
             dangerouslySetInnerHTML={(() => ({
               __html: this.props.homePageData.seasons[0].description
@@ -86,7 +86,7 @@ class HomePage extends Component<Props> {
 }
 /* istanbul ignore next */
 const mapStateToProps = state => ({
-  homePageData: get(state, ["homePage", "homePageData"])
+  homePageData: get(state, ['homePage', 'homePageData'])
 });
 
 const mapDispatchToProps = (dispatch: Map) => ({});
@@ -96,9 +96,9 @@ export default enhance(HomePage, {
   mapDispatchToProps,
   saga,
   reducer,
-  key: "homePage",
+  key: 'homePage',
   initialActions,
-  criticalState: [["homePageData"]]
+  criticalState: [['homePageData']]
 });
 
 export { HomePage as HomePageDisconnected };
--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,29 +1,21 @@
-import App, { Container } from "next/app";
-import { END } from "redux-saga";
-import React from "react";
-import { createStore, applyMiddleware, compose } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import withRedux from "next-redux-wrapper";
-import withReduxSaga from "next-redux-saga";
-import { Provider } from "react-redux";
-import createSagaMiddleware from "redux-saga";
-import enhance from "../lib/dynamicStore/enhance";
-import { withRouter } from "next/router";
-import globalReducer from "../global/reducer";
-import createReducer from "../lib/dynamicStore/reducers";
-import configureStore, { injectPageSagaReducer } from "../lib/dynamicStore/configureStore";
-import { PageMap } from "../constants";
+import App, { Container } from 'next/app';
+import { END } from 'redux-saga';
+import React from 'react';
+import withRedux from 'next-redux-wrapper';
+import { Provider } from 'react-redux';
+import configureStore, { injectPageSagaReducer } from '../lib/dynamicStore/configureStore';
+import { PageMap } from '../constants';
 
 const dynamicImport = name => {
   switch (name) {
-    case "HomePage":
-      return import("../components/templates/HomePage/HomePage.exporter.js");
+    case 'HomePage':
+      return import('../components/templates/HomePage/HomePage.exporter.js');
   }
 };
 
 const configureStoreApp = (initialState = {}) => {
   const store = configureStore({
-    initialState
+    initialState,
   });
   return store;
 };
@@ -37,26 +29,25 @@ class MyApp extends App {
       const pagePath = PageMap[pathname];
       const pageConfig = await dynamicImport(pagePath);
       // Dynamic reducer and saga
-      const reducer = pageConfig.default.reducer;
-      const saga = pageConfig.default.saga;
-      const key = pageConfig.default.key;
+      const config = pageConfig.default;
+      const { reducer, saga, key } = config;
 
       await injectPageSagaReducer({
         store,
         key,
         reducer,
-        saga
+        saga,
       });
 
       if (Component.getInitialProps) {
         pageProps = await Component.getInitialProps({ ...ctx });
       }
-      return { pageProps };
     }
+    return { pageProps };
   }
 
   render() {
-    let { Component, pageProps, store } = this.props;
+    const { Component, store } = this.props;
     return (
       <Container>
         <Provider store={store}>
